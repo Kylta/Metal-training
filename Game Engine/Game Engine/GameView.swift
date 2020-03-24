9d3ab2353fb600f6828e0317205a3caf08f584e8
cbugnon <christophe.bugnon@musicworldmedia.com>
2020-03-24 18:37:14 +0100

    Add Vertex Descriptor and clean code

--- Game Engine/Game Engine/GameView.swift
+++ Game Engine/Game Engine/GameView.swift
@@ -10,11 +10,6 @@ import MetalKit
 
 class GameView: MTKView {
     
-    struct Vertex {
-        var position: float3
-        var color: float4
-    }
-    
     private var commandQueue: MTLCommandQueue!
     private var renderPipelineState: MTLRenderPipelineState!
     
@@ -34,23 +29,6 @@ class GameView: MTKView {
         createBuffers()
     }
     
-    func createRenderPipelineState() {
-        let library = device?.makeDefaultLibrary()
-        let vertexFunction = library?.makeFunction(name: "basic_vertex_shader")
-        let fragmentFunction = library?.makeFunction(name: "basic_fragment_shader")
-        
-        let renderPipelineDescriptor = MTLRenderPipelineDescriptor()
-        renderPipelineDescriptor.colorAttachments[0].pixelFormat = .bgra8Unorm
-        renderPipelineDescriptor.vertexFunction = vertexFunction
-        renderPipelineDescriptor.fragmentFunction = fragmentFunction
-        
-        self.renderPipelineState = try! device?.makeRenderPipelineState(descriptor: renderPipelineDescriptor)
-    }
-    
-    func createBuffers() {
-        self.vertexBuffer = device?.makeBuffer(bytes: vertices, length: MemoryLayout<Vertex>.stride * vertices.count, options: [])
-    }
-    
     func createVertices() {
         self.vertices = [
             Vertex(position: float3(0, 1, 0), color: float4(1, 0, 0, 1)),       // Top midle    red
@@ -59,6 +37,38 @@ class GameView: MTKView {
         ]
     }
     
+    func createBuffers() {
+        self.vertexBuffer = device?.makeBuffer(bytes: vertices, length: Vertex.stride(vertices.count), options: [])
+    }
+    
+    func createRenderPipelineState() {
+        let library = device?.makeDefaultLibrary()
+        let vertexFunction = library?.makeFunction(name: "basic_vertex_shader")
+        let fragmentFunction = library?.makeFunction(name: "basic_fragment_shader")
+        
+        let vertexDescriptor = MTLVertexDescriptor()
+        
+        // Position
+        vertexDescriptor.attributes[0].format = .float3
+        vertexDescriptor.attributes[0].bufferIndex = 0
+        vertexDescriptor.attributes[0].offset = 0
+        
+        // Color
+        vertexDescriptor.attributes[1].format = .float4
+        vertexDescriptor.attributes[1].bufferIndex = 0
+        vertexDescriptor.attributes[1].offset = float3.size()
+        
+        vertexDescriptor.layouts[0].stride = Vertex.stride()
+        
+        let renderPipelineDescriptor = MTLRenderPipelineDescriptor()
+        renderPipelineDescriptor.colorAttachments[0].pixelFormat = .bgra8Unorm
+        renderPipelineDescriptor.vertexFunction = vertexFunction
+        renderPipelineDescriptor.fragmentFunction = fragmentFunction
+        renderPipelineDescriptor.vertexDescriptor = vertexDescriptor
+        
+        self.renderPipelineState = try! device?.makeRenderPipelineState(descriptor: renderPipelineDescriptor)
+    }
+        
     override func draw(_ dirtyRect: NSRect) {
         guard let drawable = self.currentDrawable, let renderPassDescriptor = self.currentRenderPassDescriptor else { return }
         
--- Game Engine/Game Engine/MetalTypes.swift
+++ Game Engine/Game Engine/MetalTypes.swift
@@ -6,8 +6,41 @@
 //  Copyright © 2020 Christophe Bugnon. All rights reserved.
 //
 
-import Foundation
+import simd
 
 public typealias float2 = SIMD2<Float>
 public typealias float3 = SIMD3<Float>
 public typealias float4 = SIMD4<Float>
+
+protocol Sizeable {
+    static func size(_ count: Int) -> Int
+    static func stride(_ count: Int) -> Int
+}
+
+extension Sizeable {
+    static func size() -> Int {
+        return MemoryLayout<Self>.size
+    }
+    
+    static func stride() -> Int {
+        return MemoryLayout<Self>.stride
+    }
+    
+    static func size(_ count: Int) -> Int {
+        return MemoryLayout<Self>.size * count
+    }
+    
+    static func stride(_ count: Int) -> Int {
+        return MemoryLayout<Self>.stride * count
+    }
+}
+
+
+struct Vertex {
+    var position: float3
+    var color: float4
+}
+
+extension Vertex: Sizeable {}
+
+extension float3: Sizeable {}
